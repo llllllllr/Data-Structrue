79a25d206ad08526bfda3d7ff8e8f210fbe508aa
llllllllr <35605494+llllllllr@users.noreply.github.com>
2019-04-11 09:50:56 +0800

    栈链表

---
+++ 栈/(c++类模板 数组）stack.h
@@ -0,0 +1,44 @@
+#pragma once
+#include <iostream>
+using namespace std;
+template<class T>
+class stack {
+public:
+ 
+	stack(int capacity=10 ) {
+		if (capacity < 0)
+			cout << "capacity must be bigger than 0" << endl;
+		arrayLength = capacity;
+		stk = new T[arrayLength];
+		stackTop = -1;
+
+	}
+	~stack() { delete[]stk; }
+	bool empty() { return stackTop == -1; }
+	int size() const { return stackTop + 1; }
+	T& top() {
+		if (stackTop == -1)
+		     cout << "stack is empty!" << endl;  
+		else
+			return stk[stackTop];
+	}
+	void pop() {
+		if (stackTop == -1)
+			cout << "stack is empty!cannot pop an element!" << endl;
+		else
+			stk[stackTop--].~T();
+	}
+
+	void push(const T& element) {
+		if (stackTop == arrayLength - 1)
+			cout << "Õ»ÒÑÂú" << endl;
+		else
+		 stk[++stackTop] = element;
+	}
+private:
+	int stackTop;
+	int arrayLength;
+	T* stk;
+};
+
+
---
+++ 栈/（c++模板 链表）stack.h
@@ -0,0 +1,68 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+template<class T>
+struct chainNode
+{
+	T element;
+	chainNode<T> * next;
+	chainNode(){}
+	chainNode(const T& element)
+	{
+		this->element = element;
+	}
+	chainNode(const T& element, chainNode<T> *next)
+	{
+		this->element = element;
+		this->next = next;
+	}
+};
+
+template<class T>
+class stack {
+public:
+	stack() {
+		cout << "aaaa" << endl;
+		stackSize = 0;
+		stackTop = new chainNode<T>(-1,NULL);
+	}
+	~stack() {
+		while (!stackTop != NULL)
+		{
+			chainNode<T> * next = stackTop->next;
+			delete stackTop;
+			stackTop = next;
+		}
+	}
+		bool empty(){return stackSize == 0;}
+		int size() { return stackSize; }
+		T& top() {
+			if (stackSize == 0) {
+				cout << "栈是空的，无法弹出元素" << endl;
+			}
+			else
+				return stackTop->element;
+		}
+		void push(T & element)
+		{
+			stackTop = new chainNode<T>(element, stackTop);
+			stackSize++; 
+
+		}
+		void pop() {
+			if (stackSize == 0)
+			{
+				cout << "栈是空的" << endl;
+			}
+			else {
+				chainNode<T>* nextNode = stackTop->next;
+				delete stackTop;
+				stackTop = nextNode;
+				stackSize--;
+			}
+		}
+private:
+	int stackSize;
+	chainNode<T> * stackTop;
+};
